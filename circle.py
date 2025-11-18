3e7f1002356ce8b0c3a4765fc7f88d0c9c965c8a
Tanvi Dwivedi <87585418+tdwivedi25@users.noreply.github.com>
2025-11-18 22:30:35 +0000

    completed expression

--- circle.py
+++ circle.py
@@ -1,3 +1,3 @@
 radius_input = input("Enter the radius: ")
-area = 3.14 * float(radius_input)**2
+area = 3.14 * int(radius_input)**2
 print(("Area of a circle: " + str(area) + " square units"))
--- expression.py
+++ expression.py
@@ -1,1 +1,7 @@
+def expression():
+    a = int(input("Enter the value of a: "))
+    b = int(input("Enter the value of b: "))
+    result = (a - b) + (a*b)
+    print (str(result))
 
+expression()
