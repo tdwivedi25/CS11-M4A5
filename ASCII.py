b2c342a4fad593170dbe22c616dfeaa5ea0d7471
Tanvi Dwivedi <87585418+tdwivedi25@users.noreply.github.com>
2025-11-19 01:58:17 +0000

    completed Asii

--- ASCII.py
+++ ASCII.py
@@ -1,1 +1,26 @@
+# Two ASCII art pieces stored as multiline strings
+
+cat_art = """
+ /\_/\  
+( o.o ) 
+ > ^ <
+"""
+
+dragon_art = """
+           / \\  //\\
+     |\\___/|      \\
+    /       \\     //
+   |  /\\ /\\  |   //
+   \\_\\ ° °  /_//
+     /   *   \\
+     \\__\\_/__/
+"""
+
+# Ask the user which art they want to see
+choice = input("Type 'cat' or 'dragon' to see some ASCII art: ").strip().lower()
+
+if choice == "cat":
+    print(cat_art)
+else:
+    print(dragon_art)
 
